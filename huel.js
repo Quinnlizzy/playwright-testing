545f0ab3a7055852cd2a50e667338112ff3adfb7
Quinnlizzy <quin27@hotmail.co.uk>
2024-03-29 21:32:39 +0000

    launch browser and create new pahe

--- huel.js
+++ huel.js
@@ -5,6 +5,7 @@ const playwright = require('playwright');
 async function basketTest() {
     //launch browser
     const browser = await playwright.chromium.launch({ headless: false });
+    //create new page
     const page = await browser.newPage();
     await page.goto('https://huel.com/');
   }
