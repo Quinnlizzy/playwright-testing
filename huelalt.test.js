87856e9fde667665306dd46ab2e40d37afc4b68e
Quinnlizzy <quin27@hotmail.co.uk>
2024-03-30 21:47:08 +0000

    try/catch for cookie banner

--- huelalt.test.js
+++ huelalt.test.js
@@ -3,40 +3,50 @@ import { test, expect } from '@playwright/test';
 test('completePurchaseFlow', async ({ page }) => {
     try {
         await page.goto('https://huel.com/');
-    await page.getByTestId('acceptCookieButton').click();
-    await page.getByTestId('IconLink-Search').click();
-    await page.getByTestId('SearchBar__input').click();
-    await page.getByTestId('SearchBar__input').fill('huel instant meals');
-    await page.getByTestId('SearchBar__input').press('Enter');
-    await page.getByRole('link', { name: 'Huel Instant Meals', exact: true }).click();
-    await page.getByRole('button', { name: 'Mexican Chili Increase' }).click();
-    await page.getByRole('button', { name: 'Mexican Chili Increase' }).click();
-    await page.getByRole('button', { name: 'Spicy Indian Curry Increase' }).click();
-    await page.getByRole('button', { name: 'Spicy Indian Curry Increase' }).click();
-    await page.getByRole('button', { name: 'Continue' }).click();
-    await page.waitForURL('https://huel.com/products/build-your-own-bundle?mrasn=1158041.1435750.TFBCdNT7#/step-2');
-    await page.getByRole('button', { name: 'Continue' }).click();
-    await page.getByTestId('IconLink-Search').click();
-    await page.getByTestId('SearchBar__input').fill('huel complete nutrition bar');
-    //await page.getByTestId('SearchBar__submit-button').click(); //fails on icon click?
-    await page.getByTestId('SearchBar__input').press('Enter');
-    await page.waitForTimeout(2000); // Wait for 2 seconds - couldnt get past this step without a timeout?
-    await page.getByRole('link', { name: 'Huel Complete Nutrition Bar', exact: true }).click();
-    await page.getByRole('button', { name: 'Chocolate Fudge Brownie' }).click();
-    await page.getByRole('button', { name: 'Chocolate Fudge Brownie Increase Quantity' }).click();
-    await page.getByRole('button', { name: 'Chocolate Caramel Increase' }).click();
-    await page.getByRole('button', { name: 'Chocolate Caramel Increase' }).click();
-    await page.getByRole('button', { name: 'Dark Chocolate Raspberry' }).click();
-    await page.getByRole('button', { name: 'Continue' }).click();
-    await page.getByTestId('IconLink-Cart').click();
-    const url = page.url();
-        expect(url).toBe('https://huel.com/cart');
-    await page.locator('span').filter({ hasText: 'Add Free T-shirt' }).click();
-    await page.getByText('XL', { exact: true }).click();
-    await page.locator('.tshirt-modal__black-square').click();
-    await page.locator('span').filter({ hasText: '.st0{fill:none;stroke:#0B0B0B;stroke-width:20;stroke-miterlimit:10;} Men\'s' }).locator('#Layer_1').click();
-    await page.getByRole('button', { name: 'Add Free T-shirt' }).click();
-    await page.getByRole('button', { name: 'Secure Checkout' }).click();
+        const url = page.url();
+        expect(url).toBe('https://huel.com/');
+        await page.getByTestId('acceptCookieButton').click();
+            let errorOccurred = false;
+            try {
+                await page.getByTestId('cookieBanner');
+            } catch (error) {
+                errorOccurred = true;
+            }
+            expect(errorOccurred).toBeTruthy();
+        await page.getByTestId('IconLink-Search').click();
+        await page.getByTestId('SearchBar__input').click();
+        await page.getByTestId('SearchBar__input').fill('huel instant meals');
+        await page.getByTestId('SearchBar__input').press('Enter');
+        await page.getByRole('link', { name: 'Huel Instant Meals', exact: true }).click();
+        await page.getByRole('button', { name: 'Mexican Chili Increase' }).click();
+        await page.getByRole('button', { name: 'Mexican Chili Increase' }).click();
+        await page.getByRole('button', { name: 'Spicy Indian Curry Increase' }).click();
+        await page.getByRole('button', { name: 'Spicy Indian Curry Increase' }).click();
+        await page.getByRole('button', { name: 'Continue' }).click();
+        //await page.waitForURL('https://huel.com/products/build-your-own-bundle?mrasn=1158041.1435750.TFBCdNT7#/step-2');
+        //worked better without
+        await page.getByRole('button', { name: 'Continue' }).click();
+        await page.getByRole('button', { name: 'Continue to Cart' }).click();
+        await page.getByTestId('IconLink-Search').click();
+        await page.getByTestId('SearchBar__input').fill('huel complete nutrition bar');
+        //await page.getByTestId('SearchBar__submit-button').click(); //fails on icon click?
+        await page.waitForTimeout(2000);
+        await page.getByTestId('SearchBar__input').press('Enter');
+        await page.waitForTimeout(2000); // Wait for 2 seconds - couldnt get past this step without a timeout?
+        await page.getByRole('link', { name: 'Huel Complete Nutrition Bar', exact: true }).click();
+        await page.getByRole('button', { name: 'Chocolate Fudge Brownie' }).click();
+        await page.getByRole('button', { name: 'Chocolate Fudge Brownie Increase Quantity' }).click();
+        await page.getByRole('button', { name: 'Chocolate Caramel Increase' }).click();
+        await page.getByRole('button', { name: 'Chocolate Caramel Increase' }).click();
+        await page.getByRole('button', { name: 'Dark Chocolate Raspberry' }).click();
+        await page.getByRole('button', { name: 'Continue' }).click();
+        await page.getByTestId('IconLink-Cart').click();
+        await page.locator('span').filter({ hasText: 'Add Free T-shirt' }).click();
+        await page.getByText('XL', { exact: true }).click();
+        await page.locator('.tshirt-modal__black-square').click();
+        await page.locator('span').filter({ hasText: '.st0{fill:none;stroke:#0B0B0B;stroke-width:20;stroke-miterlimit:10;} Men\'s' }).locator('#Layer_1').click();
+        await page.getByRole('button', { name: 'Add Free T-shirt' }).click();
+        await page.getByRole('button', { name: 'Secure Checkout' }).click();
 } catch (error) {
     console.error(`Test failed with error: ${error}`);
 }
